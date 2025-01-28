981028c7adcba32ccbbc36be4ee633c928e043ae
Davi <dp.junior1997@gmail.com>
2025-01-28 17:32:37 -0300

    feat: add react router dom setup and homePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ToastContainer, Zoom } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 
@@ -8,19 +9,21 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToastContainer
-      position="bottom-center"
-      autoClose={2500}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick={false}
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-      transition={Zoom}
-    />
-    <App />
+    <BrowserRouter>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Zoom}
+      />
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
