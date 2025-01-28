94ffd1b1db90e80b60ff8d723ce909f455d4906d
Davi <dp.junior1997@gmail.com>
2025-01-28 17:36:26 -0300

    feat: add Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import CustomButton from "../components/CustomButton";
+
+import "./Login.scss";
+import logo from "../assets/logo.svg";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <img src={logo} alt="Davi Pereira Web Developer" />
+      <div className="button-container">
+        <CustomButton>Entrar</CustomButton>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
